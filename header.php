a22fb5df3003609c43dfebfd9dcab8207328cc85
bubetic <bubetici@gmail.com>
2024-03-21 14:03:32 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="csrf_token" content="<?php echo createToken(); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="icon" href="dmsite.png">
+  <title>dusanmilovanovic</title>
+</head>
+
+<body>
+  <main>
+    <ul>
+      <li><a class="active" href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="login.html">Login</a></li>
+    </ul>
