26ec73a1ca68415e0a600c1f8820dcea87bc3b02
bubetic <bubetici@gmail.com>
2024-04-03 09:10:11 +0200

    Update header

--- header.php
+++ header.php
@@ -17,6 +17,6 @@
       <li><a href="#news">News</a></li>
       <li><a href="#contact">Contact</a></li>
       <li><a href="#about">About</a></li>
-      <li><a href="#signup.php">Sign Up</a></li>
-      <li><a href="#login.html">Login</a></li>
+      <li><a href="#signup">Sign Up</a></li>
+      <li><a href="#login">Login</a></li>
     </ul>
