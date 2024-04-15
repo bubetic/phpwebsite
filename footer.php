1e071fcedce32633388fdc5f9f2a6038f09e943d
bubetic <bubetici@gmail.com>
2024-04-15 17:41:21 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <div class="footer">
       <footer>
-        <p class="p1">dusanmilovanovic.com <?php echo date('Y'); ?></p>
+        <p class="p1">dusanmilovanovic.com © <?php echo date('Y'); ?></p>
       </footer>
     </div>
   </main>
