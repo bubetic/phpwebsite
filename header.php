fccaccc7110f648faa6096f786598a7ff2c4a732
bubetic <bubetic@users.noreply.github.com>
2025-12-11 18:51:47 +0100

    added CSRF token to header.php and added utils.php to header.php

--- header.php
+++ header.php
@@ -1,9 +1,12 @@
+<?php
+require_once 'php/utils.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
-  <meta name="csrf_token" content="" />
+  <meta name="csrf_token" content="<?php echo createToken(); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <link rel="icon" href="dmsite.png">
@@ -13,10 +16,10 @@
 <body>
   <main>
     <ul>
-      <li><a class="active" href="#home">Home</a></li>
+      <li><a class="active" href="index.php">Home</a></li>
       <li><a href="#news">News</a></li>
       <li><a href="#contact">Contact</a></li>
       <li><a href="#about">About</a></li>
-      <li><a href="#signup">Sign Up</a></li>
-      <li><a href="#login">Login</a></li>
+      <li><a href="register.php">Sign Up</a></li>
+      <li><a href="login.php">Login</a></li>
     </ul>
