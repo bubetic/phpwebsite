87c86e0de74d1d88e6354ed7c8b67b9a54f5d6bf
bubetic <bubetic@users.noreply.github.com>
2026-03-29 17:49:42 +0200

    update register.php delete index.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php 
-	//require_once 'php/utils.php';
-	//$csrfToken = createToken();
+	require_once 'php/utils.php';
+	$csrfToken = createToken();
 	include_once 'header.php';
 ?>
 	<div class="formWrapper">
